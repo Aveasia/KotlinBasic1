2342f00eebeb034211ec651149ebdf8b43b667b4
Aveasia <paramec1@gmail.com>
2024-01-18 14:47:30 +0300

    KS-1-3

---
+++ src/main/kotlin/Lesson_1/lesson1_task3.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val yearTakeoffJG: Short = 1961
+    var hourTakeoffJG: Int = 9
+    var minuteTakeoffJG: Int = 7
+
+    println(yearTakeoffJG)
+    println(hourTakeoffJG)
+    println(minuteTakeoffJG)
+
+    /* я все пытался вывести 09 и 07 на экран таким способом, но потом понял, что скорее всего не это от меня требуется
+    var nn: String = ("09")
+     print(nn)
+    var ff: String = ("07")
+    print("nn:ff") но опять же не дает вывести двоеточие я в тупике :D
+*/
+
+    hourTakeoffJG = 10
+    minuteTakeoffJG = 55
+
+    print(hourTakeoffJG)
+    print(minuteTakeoffJG)
