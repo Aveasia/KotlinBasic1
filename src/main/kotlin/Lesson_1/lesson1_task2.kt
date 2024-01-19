7623012cdae2218b3744f8f7825eda9f93353dbb
Aveasia <paramec1@gmail.com>
2024-01-19 16:01:19 +0300

    KS-1-2 refactor

---
+++ src/main/kotlin/Lesson_1/lesson1_task2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val allOrders: Int = 75
+    val thxForPurchase: String = ("Благодарим за покупку!")
+    val thxForPurNext: String = (" Ждем Вас снова =)")
+    println(allOrders)
+    println(thxForPurchase)
+    println(thxForPurNext)
+    var numberOfEmployees: Short = 2000
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
